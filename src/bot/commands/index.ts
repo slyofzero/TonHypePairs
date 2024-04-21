a4a9cf6c7cfd5e231a4d4e2debeeefff6c719b92
Ishan Shishodiya <sly.of.zero@gmail.com>
2024-04-21 11:28:48 +0530

    fixes

--- src/bot/commands/index.ts
+++ src/bot/commands/index.ts
@@ -1,13 +1,16 @@
 import { teleBot } from "@/index";
 import { startBot } from "./start";
-import { log } from "@/utils/handlers";
+import { errorHandler, log } from "@/utils/handlers";
 import { sponsor } from "./sponsor";
 
 export function initiateBotCommands() {
-  teleBot.api.setMyCommands([
-    { command: "start", description: "Start the bot" },
-    { command: "sponsor", description: "Add a sponsor text" },
-  ]);
+  teleBot.api
+    .setMyCommands([
+      { command: "start", description: "Start the bot" },
+      { command: "sponsor", description: "Add a sponsor text" },
+    ])
+    .catch((e) => errorHandler(e));
+
   teleBot.command("start", (ctx) => startBot(ctx));
   teleBot.command("sponsor", (ctx) => sponsor(ctx));
   log("Bot commands up");
